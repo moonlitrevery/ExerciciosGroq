229fd62dfa7669e9be2ea3e2ee482757a86303e1
João Vitor <105260702+moonlitrevery@users.noreply.github.com>
2026-03-11 20:11:11 -0300

    initial commit with exercises 1,2 and 3

---
+++ exercicios.py
@@ -0,0 +1,164 @@
+import os
+import json
+from openai import OpenAI
+from tools import *
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq()
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "somar",
+            "description": "Soma dois números",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "a": {
+                        "type": "integer",
+                        "description": "Primeiro número"
+                    },
+                    "b": {
+                        "type": "integer",
+                        "description": "Segundo número"
+                    }
+                },
+                "required": ["a", "b"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "multiplicar",
+            "description": "Multiplica dois números",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "a": {
+                        "type": "integer",
+                        "description": "Primeiro número"
+                    },
+                    "b": {
+                        "type": "integer",
+                        "description": "Segundo número"
+                    }
+                },
+                "required": ["a", "b"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "dividir",
+            "description": "Divide dois números",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "a": {
+                        "type": "integer",
+                        "description": "Primeiro número"
+                    },
+                    "b": {
+                        "type": "integer",
+                        "description": "Segundo número"
+                    }
+                },
+                "required": ["a", "b"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "subtrair",
+            "description": "Subtrai dois números",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "a": {
+                        "type": "integer",
+                        "description": "Primeiro número"
+                    },
+                    "b": {
+                        "type": "integer",
+                        "description": "Segundo número"
+                    }
+                },
+                "required": ["a", "b"]
+            }   
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "converter_celsius_para_fahrenheit",
+            "description": "Converte Celsius para Fahrenheit",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "celsius": {
+                        "type": "number",
+                        "description": "Temperatura em Celsius"
+                    }
+                },
+                "required": ["celsius"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "converter_fahrenheit_para_celsius",
+            "description": "Converte Fahrenheit para Celsius",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "fahrenheit": {
+                        "type": "number",
+                        "description": "Temperatura em Fahrenheit"
+                    }
+                },
+                "required": ["fahrenheit"]
+            }  
+        }
+    }
+]
+
+def perguntar(pergunta: str):
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=[
+            {"role": "system", "content": "Você é um assistente que decide qual função usar."},
+            {"role": "user", "content": pergunta}
+        ],
+        tools=tools,
+        tool_choice="auto",
+        temperature=0
+    )
+    message = response.choices[0].message
+    if message.tool_calls:
+        tool_call = message.tool_calls[0]
+        tool_name = tool_call.function.name
+        args = json.loads(tool_call.function.arguments)
+
+    if tool_name == "somar":
+        return somar(**args)
+    elif tool_name == "multiplicar":
+        return multiplicar(**args)
+    elif tool_name == "dividir":
+        return dividir(**args)
+    elif tool_name == "subtrair":
+        return subtrair(**args)
+    elif tool_name == "converter_celsius_para_fahrenheit":
+        return converter_celsius_para_fahrenheit(**args)
+    elif tool_name == "converter_fahrenheit_para_celsius":
+        return converter_fahrenheit_para_celsius(**args)
+    return message.content
+
+print(perguntar(input("Digite uma pergunta: ")))
---
+++ tools.py
@@ -0,0 +1,17 @@
+def somar(a, b):
+    return a + b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    return a / b
+
+def subtrair(a, b):
+    return a - b
+
+def converter_celsius_para_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def converter_fahrenheit_para_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
