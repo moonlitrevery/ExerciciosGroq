2f0aef1d162f23c023a8371c44ba3918c3ca8de9
João Vitor <105260702+moonlitrevery@users.noreply.github.com>
2026-03-11 20:22:07 -0300

    feat: exercises 4 and 5 (on tools this time lmao)

--- tools.py
+++ tools.py
@@ -15,3 +15,19 @@ def converter_celsius_para_fahrenheit(celsius):
 
 def converter_fahrenheit_para_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
+
+def buscar_produtos(nome):
+    produtos = {
+    "notebook": 4500,
+    "mouse": 80,
+    "teclado": 150
+}
+    return produtos.get(nome, "Produto não encontrado")
+
+def buscar_estoque(nome):
+    estoque = {
+    "notebook": 5,
+    "mouse": 20,
+    "teclado": 8
+}
+    return estoque.get(nome, "Produto não encontrado")
